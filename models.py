1cdce3cec7eb469030bd1c7a6bc49b3853d42d14
akshaybob <96284771+akshaybob@users.noreply.github.com>
2024-06-10 17:24:47 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,10 @@
-import os
+"""Module delas with Table Model structure """
 import uuid
 from sqlalchemy import create_engine, Column, String, Date, DateTime, Enum, Text, Integer, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from datetime import datetime
-from pydantic import BaseModel as PydanticBaseModel, Field
-from typing import Optional
-from dotenv import load_dotenv
 import enum
-
-load_dotenv()
-
-Base = declarative_base()
+from database import Base
 
 
 class TaskStatus(enum.Enum):
@@ -43,37 +36,4 @@ class LegitimateSeller(Base):
     date = Column(Date, default=datetime.utcnow().date())
     run_id = Column(String(200), ForeignKey('tasks.run_id'))# 30
     task = relationship('Task', back_populates='legitimate_sellers')
-
-
-DATABASE_URL =os.getenv('DATABASE_URL')
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-
-class TaskModel(PydanticBaseModel):
-    run_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
-    date: datetime = Field(default_factory=datetime.utcnow)
-    status: str
-    error: Optional[str] = None
-    started_at: Optional[datetime] = None
-    finished_at: Optional[datetime] = None
-    failed_at: Optional[datetime] = None
-
-    class Config:
-        from_attributes = True
-
-
-class LegitimateSellerModel(PydanticBaseModel):
-    id: int
-    site: str
-    ssp_domain_name: str
-    publisher_id: str
-    seller_relationship: str
-    date: datetime = Field(default_factory=datetime.utcnow)
-    run_id: str
-
-    class Config:
-        from_attributes = True
+    
